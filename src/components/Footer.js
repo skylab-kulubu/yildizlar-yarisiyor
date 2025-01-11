a57951c5cdf261542238ce6d42ed31c85f290343
Emre Uslu <75951256+rakun256@users.noreply.github.com>
2025-01-11 11:58:24 +0300

    Footer SKY LAB

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,8 @@ import linkedinIcon from "../images/linkedin.png";
 import discordIcon from "../images/discord.png";
 import xIcon from "../images/x.png";
 import crossLogo from "../images/cross.png";
+import skylab from "../images/skylab.png";
+import weblab from "../images/weblab-color.png";
 
 const Footer = () => {
   return (
@@ -12,18 +14,32 @@ const Footer = () => {
       <div className="container mx-auto flex w-full justify-between items-center">
         {/* Sol - Sosyal Medya */}
         <div className="flex flex-col items-center justify-center">
-          <p className="text-dark-accentpurple font-bold mb-4 text-2xl">Bizi Takip Edin</p>
+          <p className="text-dark-accentpurple font-bold mb-4 text-2xl">
+            Bizi Takip Edin
+          </p>
           <div className="grid grid-cols-2 gap-8">
-            <a href="instagram.com" className="w-12 h-12 flex items-center justify-center rounded">
+            <a
+              href="instagram.com"
+              className="w-12 h-12 flex items-center justify-center rounded"
+            >
               <img src={instaIcon} alt="Instagram" className="w-12 h-12" />
             </a>
-            <a href="linkedin.com" className="w-12 h-12 flex items-center justify-center rounded">
+            <a
+              href="linkedin.com"
+              className="w-12 h-12 flex items-center justify-center rounded"
+            >
               <img src={linkedinIcon} alt="LinkedIn" className="w-12 h-12" />
             </a>
-            <a href="x.com" className="w-12 h-12 flex items-center justify-center rounded">
+            <a
+              href="x.com"
+              className="w-12 h-12 flex items-center justify-center rounded"
+            >
               <img src={xIcon} alt="X" className="w-12 h-12" />
             </a>
-            <a href="discord.com" className="w-12 h-12 flex items-center justify-center rounded">
+            <a
+              href="discord.com"
+              className="w-12 h-12 flex items-center justify-center rounded"
+            >
               <img src={discordIcon} alt="Discord" className="w-12 h-12" />
             </a>
           </div>
@@ -32,7 +48,11 @@ const Footer = () => {
         {/* Orta - Sponsorlar */}
         <div className="flex flex-col items-center justify-center">
           {/* Büyük Cross */}
-          <img src={crossLogo} alt="Cross Logo" className="w-32 h-32 mb-4 rounded-full" />
+          <img
+            src={crossLogo}
+            alt="Cross Logo"
+            className="w-32 h-32 mb-4 rounded-full"
+          />
           {/* Küçük Cross Logolar */}
           <div className="flex space-x-4 justify-center items-center">
             {[...Array(5)].map((_, index) => (
@@ -48,8 +68,35 @@ const Footer = () => {
 
         {/* Sağ - YTÜMK Logo */}
         <div className="flex items-center justify-end">
-          <img src={ytumLogo} alt="YTÜMK Logo" className="w-32 h-32 rounded-full" />
+          <img
+            src={ytumLogo}
+            alt="YTÜMK Logo"
+            className="w-32 h-32 rounded-full"
+          />
         </div>
+      </div>
+
+      <div className="mt-16 text-center">
+        <div className="flex justify-center space-x-8 mt-2">
+          <a
+            href="https://yildizskylab.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={skylab} alt="SkyLab" className="w-12 h-12" />
+          </a>
+          <a
+            href="https://yildizskylab.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={weblab} alt="WebLab" className="w-12 h-12" />
+          </a>
+        </div>
+        <p className="text-md mt-4">
+          SKY LAB: Bilgisayar Bilimleri Kulübü WebLab tarafından
+          geliştirilmiştir.
+        </p>
       </div>
     </footer>
   );
